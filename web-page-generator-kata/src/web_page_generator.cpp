8d319008c137662a89ac9b1cdf490abec21a5578
Luis Rovirosa <luisrovirosa@gmail.com>
2019-03-25 14:47:03 +0100

    WebPageGenerator creates a user.html file

--- web-page-generator-kata/src/web_page_generator.cpp
+++ web-page-generator-kata/src/web_page_generator.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
+#include <fstream>
+
 using namespace std;
 
 int main( int, char* [] )
 {
-    cout << "Hello world\n";
+    char filename[ ] = "./html/users.html";
+    fstream appendFileToWorkWith;
+
+    appendFileToWorkWith.open(filename, std::fstream::out);
+
+    appendFileToWorkWith << "<html></html>";
+    appendFileToWorkWith.close();
     return 0;
 }
